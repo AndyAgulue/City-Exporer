40e9bbf9ee1dfd293d4079ca1996c7fdfc629967
Andy Agulue <andyfandandy@gmail.com>
2021-02-24 20:59:28 -0800

    initial sql table

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE city_explorer_table;
+
+CREATE TABLE city_explorer_table (
+  id  SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  latitude NUMERIC(10,7),
+  longitude NUMERIC(10,7)
+)
